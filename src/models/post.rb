76e19bf1e006aa14a68a6df6cbcfca11d4dff2b6
Philip Ross <dev@baseballlover723.com>
2026-07-05 15:25:37 -0700

    fix model relations

--- src/models/post.rb
+++ src/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
   self.table_name = 'posts'
 
-  belongs_to :author, class_name: 'User', optional: true
+  belongs_to :user, primary_key: "username", foreign_key: "author", optional: true
   has_many :comments, dependent: :destroy
   has_many :mod_actions, foreign_key: "target_post_id", dependent: :destroy
   has_one :flair_frequency_exemption, dependent: :destroy
